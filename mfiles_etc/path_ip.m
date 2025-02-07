7d5e476f404a7c83ac251f112998b723c586a05e
Prof-Carter <106240356+Prof-Carter@users.noreply.github.com>
2025-02-07 15:23:04 +0900

    Update path_ip.m

--- mfiles_etc/path_ip.m
+++ mfiles_etc/path_ip.m
@@ -1,4 +1,4 @@
 addpath(genpath('C:\hoge\ip_toolbox_1.0.2\iptools'))
 addpath(genpath('C:\hoge\ip_toolbox_1.0.2\odqlab_2.1.3'))
-addpath(genpath('C:\hoge\sedumi-master'))
+addpath(genpath('C:\hoge\sedumi'))
 addpath(genpath('C:\hoge\YALMIP-master'))
